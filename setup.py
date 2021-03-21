03b830e2802f123edff29cda2886512eda30a478
Alp Niksarlı <alp.niksarli@gmail.com>
2021-03-22 01:29:56 +0300

    initial commit

---
+++ setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+VERSION = '0.1.11'
+DESCRIPTION = 'Scrape most popular websites easily'
+LONG_DESCRIPTION = 'This is a Python package that helps with scraping some of the most popular websites such as Wikipedia, Amazon, etc.'
+
+# Setting up
+setup(
+    name="scrapista",
+    version=VERSION,
+    author="Alp Niksarlı",
+    author_email="<alp.niksarli@gmail.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=find_packages(),
+    install_requires=['requests', 'bs4'],
+    keywords=['python', 'scrape', 'amazon', 'wikipedia', 'web', 'data mining', 'web scraping'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
