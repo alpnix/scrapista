4ab0afa68d62d016821aac8d0e7b2416edb56f99
Alp Niksarlı <72332955+alpnix@users.noreply.github.com>
2021-03-22 11:46:41 +0300

    update config

--- setup.py
+++ setup.py
@@ -3,11 +3,16 @@ import codecs
 import os
 
 here = os.path.abspath(os.path.dirname(__file__))
+__version__ = find_version('scrapista/__init__.py')
 
 with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
     long_description = "\n" + fh.read()
 
-VERSION = '0.1.11'
+VERSION = __version__
+REQUIREMENTS = [
+    'requests', 
+    'bs4'
+]
 DESCRIPTION = 'Scrape most popular websites easily'
 LONG_DESCRIPTION = 'This is a Python package that helps with scraping some of the most popular websites such as Wikipedia, Amazon, etc.'
 
@@ -15,20 +20,24 @@ LONG_DESCRIPTION = 'This is a Python package that helps with scraping some of th
 setup(
     name="scrapista",
     version=VERSION,
+    license='MIT',
     author="Alp Niksarlı",
-    author_email="<alp.niksarli@gmail.com>",
+    author_email="alp.niksarli@gmail.com",
+    url='https://github.com/alpnix/scrapista',
     description=DESCRIPTION,
     long_description_content_type="text/markdown",
     long_description=long_description,
     packages=find_packages(),
-    install_requires=['requests', 'bs4'],
-    keywords=['python', 'scrape', 'amazon', 'wikipedia', 'web', 'data mining', 'web scraping'],
+    install_requires=,
     classifiers=[
         "Development Status :: 1 - Planning",
         "Intended Audience :: Developers",
+        'License :: MIT License',
         "Programming Language :: Python :: 3",
         "Operating System :: Unix",
         "Operating System :: MacOS :: MacOS X",
         "Operating System :: Microsoft :: Windows",
-    ]
+        "Topic :: Web Scraping :: Data Mining",
+    ],
+    keywords=['python', 'scrape', 'amazon', 'wikipedia', 'web', 'data mining', 'web scraping']
 )
