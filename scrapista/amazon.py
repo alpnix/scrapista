b23a87ab60ada7ae94598452502be266b2efcc42
Alp Niksarlı <alp.niksarli@gmail.com>
2021-03-24 22:10:59 +0300

    amazon scraper class initial version

--- scrapista/amazon.py
+++ scrapista/amazon.py
@@ -1,5 +1,6 @@
 from bs4 import BeautifulSoup
 from time import perf_counter
+import concurrent.futures
 import threading as th
 import requests
 import re
@@ -85,7 +86,7 @@ class AmazonScraper:
             for each element in that list and return it in json form
         """
 
-        if type(keywords) != "list": 
+        if type(keywords) != list: 
             raise BaseException("A list type is expected for 'keywords' argument")
 
 
@@ -102,7 +103,7 @@ class AmazonScraper:
             but this one runs with multiprocessing therefore it's much faster
         """
 
-        if type(keywords) != "list": 
+        if type(keywords) != list: 
             raise BaseException("A list type is expected for 'keywords' argument")
 
         threads = [th.Thread(target=self.scrape_keyword,args=(keyword,data_list)) for keyword in keywords]
@@ -143,14 +144,12 @@ class AmazonScraper:
         try: 
             title = title_tag.get_text("|",strip=True)
         except Exception as e: 
-            print(e)
             title = None
 
         try: 
             stars = stars_tag.get_text("|",strip=True)[:3].replace(",",".")
             stars = float(stars)
         except Exception as e: 
-            print(e)
             stars = None
         
         try: 
@@ -158,10 +157,8 @@ class AmazonScraper:
             price = price.replace(",",".")
             price_list = price.split(".")
             price = price_list[0] + "." + price_list[1][:2]
-            price = float(price)
-            
+            price = float(price)            
         except Exception as e: 
-            print(e)
             price = None
 
 
@@ -206,8 +203,21 @@ class AmazonScraper:
             AmazonScraper.track_items function 
         """
 
-        if type(urls) != "list": 
+        if type(urls) != list: 
             raise BaseException("A list type is expected for 'urls' argument")
+
+
+
+        with concurrent.futures.ThreadPoolExecutor() as executor:
+            for url in urls: 
+                future = executor.submit(self.track_item,url)
+                data = future.result()
+                data_list.append(data)
+
+
+        data_list = list(filter(lambda x: any(x.values()), data_list)) 
+
+        return data_list
 
 
 
@@ -216,7 +226,7 @@ headers = {
 }
 
 items = [
-    "pencil","cardboard","racket","paddle","book","headphone","water bottle","usb charger","book shelf","paper","markers","pencilcase","notebook","calculator","braces","iphone","apple watch","bluetooth charger","airpods"
+    "pencil","cardboard","racket","paddle","book","headphone","water bottle","usb charger","book shelf","paper","markers","pencilcase","notebook","calculator"
 ]
 
 start = perf_counter()
@@ -228,12 +238,16 @@ scraper = AmazonScraper(headers)
 
 # print(data)
 
+"Scraping multiple keywords"
+"Sync scraping keywords"
+
+# data_list = scraper.scrape_keywords(items)
+# print(len(data_list))
 
 "Async scraping keywords"
 
-# data_list = scraper.async_scrape_keywords(items)
-
-# print(len(data_list))
+data_list = scraper.async_scrape_keywords(items)
+print(len(data_list))
 
 "Tracking item"
 
@@ -243,15 +257,17 @@ scraper = AmazonScraper(headers)
 
 "Tracking multiple items"
 
-urls = ["https://www.amazon.de/-/en/Lenovo-IdeaPad-Chromebook-WideView-Tablet/dp/B08CZXFKDP/?_encoding=UTF8&pd_rd_w=wofLG&pf_rd_p=07208ea4-5452-4c67-8f65-44901c0f68eb&pf_rd_r=0JJ8ZD05AEXFSZAQB3TS&pd_rd_r=8c4d6402-ff5e-4ee0-b158-13f872c81f7d&pd_rd_wg=BfT3H&ref_=pd_gw_ci_mcx_mr_hp_d","https://www.amazon.de/Window-Battery-Charger-Supply-Karcher/dp/B07WCWHXRG/ref=sr_1_4_sspa?dchild=1&keywords=tuch&qid=1616610242&sr=8-4-spons&psc=1&spLa=ZW5jcnlwdGVkUXVhbGlmaWVyPUEyQUpJVk5aM0dLVExVJmVuY3J5cHRlZElkPUEwNzU0MTk3SThHNlk4M0xJQTNaJmVuY3J5cHRlZEFkSWQ9QTAyNzgxNzkyWkJBNlg4S0pPVkU0JndpZGdldE5hbWU9c3BfYXRmJmFjdGlvbj1jbGlja1JlZGlyZWN0JmRvTm90TG9nQ2xpY2s9dHJ1ZQ==","https://www.amazon.de/-/en/Heinz-Drache/dp/B014STN07I/ref=sr_1_12?dchild=1&keywords=tuch&qid=1616610258&sr=8-12"]
+urls = [
+    "https://www.amazon.de/-/en/Lenovo-IdeaPad-Chromebook-WideView-Tablet/dp/B08CZXFKDP/?_encoding=UTF8&pd_rd_w=wofLG&pf_rd_p=07208ea4-5452-4c67-8f65-44901c0f68eb&pf_rd_r=0JJ8ZD05AEXFSZAQB3TS&pd_rd_r=8c4d6402-ff5e-4ee0-b158-13f872c81f7d&pd_rd_wg=BfT3H&ref_=pd_gw_ci_mcx_mr_hp_d","https://www.amazon.de/Window-Battery-Charger-Supply-Karcher/dp/B07WCWHXRG/ref=sr_1_4_sspa?dchild=1&keywords=tuch&qid=1616610242&sr=8-4-spons&psc=1&spLa=ZW5jcnlwdGVkUXVhbGlmaWVyPUEyQUpJVk5aM0dLVExVJmVuY3J5cHRlZElkPUEwNzU0MTk3SThHNlk4M0xJQTNaJmVuY3J5cHRlZEFkSWQ9QTAyNzgxNzkyWkJBNlg4S0pPVkU0JndpZGdldE5hbWU9c3BfYXRmJmFjdGlvbj1jbGlja1JlZGlyZWN0JmRvTm90TG9nQ2xpY2s9dHJ1ZQ==","https://www.amazon.de/-/en/Heinz-Drache/dp/B014STN07I/ref=sr_1_12?dchild=1&keywords=tuch&qid=1616610258&sr=8-12","https://www.amazon.de/-/en/Lenovo-IdeaPad-Chromebook-WideView-Tablet/dp/B08CZXFKDP/?_encoding=UTF8&pd_rd_w=wofLG&pf_rd_p=07208ea4-5452-4c67-8f65-44901c0f68eb&pf_rd_r=0JJ8ZD05AEXFSZAQB3TS&pd_rd_r=8c4d6402-ff5e-4ee0-b158-13f872c81f7d&pd_rd_wg=BfT3H&ref_=pd_gw_ci_mcx_mr_hp_d","https://www.amazon.de/Window-Battery-Charger-Supply-Karcher/dp/B07WCWHXRG/ref=sr_1_4_sspa?dchild=1&keywords=tuch&qid=1616610242&sr=8-4-spons&psc=1&spLa=ZW5jcnlwdGVkUXVhbGlmaWVyPUEyQUpJVk5aM0dLVExVJmVuY3J5cHRlZElkPUEwNzU0MTk3SThHNlk4M0xJQTNaJmVuY3J5cHRlZEFkSWQ9QTAyNzgxNzkyWkJBNlg4S0pPVkU0JndpZGdldE5hbWU9c3BfYXRmJmFjdGlvbj1jbGlja1JlZGlyZWN0JmRvTm90TG9nQ2xpY2s9dHJ1ZQ==","https://www.amazon.de/-/en/Heinz-Drache/dp/B014STN07I/ref=sr_1_12?dchild=1&keywords=tuch&qid=1616610258&sr=8-12","https://www.amazon.de/-/en/Lenovo-IdeaPad-Chromebook-WideView-Tablet/dp/B08CZXFKDP/?_encoding=UTF8&pd_rd_w=wofLG&pf_rd_p=07208ea4-5452-4c67-8f65-44901c0f68eb&pf_rd_r=0JJ8ZD05AEXFSZAQB3TS&pd_rd_r=8c4d6402-ff5e-4ee0-b158-13f872c81f7d&pd_rd_wg=BfT3H&ref_=pd_gw_ci_mcx_mr_hp_d","https://www.amazon.de/Window-Battery-Charger-Supply-Karcher/dp/B07WCWHXRG/ref=sr_1_4_sspa?dchild=1&keywords=tuch&qid=1616610242&sr=8-4-spons&psc=1&spLa=ZW5jcnlwdGVkUXVhbGlmaWVyPUEyQUpJVk5aM0dLVExVJmVuY3J5cHRlZElkPUEwNzU0MTk3SThHNlk4M0xJQTNaJmVuY3J5cHRlZEFkSWQ9QTAyNzgxNzkyWkJBNlg4S0pPVkU0JndpZGdldE5hbWU9c3BfYXRmJmFjdGlvbj1jbGlja1JlZGlyZWN0JmRvTm90TG9nQ2xpY2s9dHJ1ZQ==","https://www.amazon.de/-/en/Heinz-Drache/dp/B014STN07I/ref=sr_1_12?dchild=1&keywords=tuch&qid=1616610258&sr=8-12","https://www.amazon.de/-/en/Lenovo-IdeaPad-Chromebook-WideView-Tablet/dp/B08CZXFKDP/?_encoding=UTF8&pd_rd_w=wofLG&pf_rd_p=07208ea4-5452-4c67-8f65-44901c0f68eb&pf_rd_r=0JJ8ZD05AEXFSZAQB3TS&pd_rd_r=8c4d6402-ff5e-4ee0-b158-13f872c81f7d&pd_rd_wg=BfT3H&ref_=pd_gw_ci_mcx_mr_hp_d","https://www.amazon.de/Window-Battery-Charger-Supply-Karcher/dp/B07WCWHXRG/ref=sr_1_4_sspa?dchild=1&keywords=tuch&qid=1616610242&sr=8-4-spons&psc=1&spLa=ZW5jcnlwdGVkUXVhbGlmaWVyPUEyQUpJVk5aM0dLVExVJmVuY3J5cHRlZElkPUEwNzU0MTk3SThHNlk4M0xJQTNaJmVuY3J5cHRlZEFkSWQ9QTAyNzgxNzkyWkJBNlg4S0pPVkU0JndpZGdldE5hbWU9c3BfYXRmJmFjdGlvbj1jbGlja1JlZGlyZWN0JmRvTm90TG9nQ2xpY2s9dHJ1ZQ==","https://www.amazon.de/-/en/Heinz-Drache/dp/B014STN07I/ref=sr_1_12?dchild=1&keywords=tuch&qid=1616610258&sr=8-12"
+]
 
+"Synchronously tracking multiple items"
+# data_list = scraper.track_items(urls)
+# print(data_list)
 
-data_list = scraper.track_items(urls)
-
-print(data_list)
-
-
-
+"Asynchronously tracking multiple items"
+# data_list = scraper.async_track_items(urls)
+# print(data_list)
 
 
 end = perf_counter()
